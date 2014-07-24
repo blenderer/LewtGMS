a6230b12ef1ff47a0aa8aa14055c6bb04a745d90
Eric Harrison <eric.h@rrison.io>
2014-07-24 01:51:01 -0400

    Initial File transfer

---
+++ js/job.js
@@ -0,0 +1,87 @@
+$(function() {
+	jobList = [];
+	statList = [];
+	$("#fileupload").change(function() {
+		var f = $(this).prop("files")[0];
+	    var r = new FileReader();
+	    r.onload = function() {
+	        var Uints = new Uint8Array(r.result);
+	        db = new SQL.Database(Uints);
+
+	        //LOAD JOBS
+	        var stmt = db.prepare("SELECT * FROM Jobs");
+			stmt.getAsObject();
+			
+			while(stmt.step()) { //
+		        var row = stmt.getAsObject();
+		        row.statpriority = row.statpriority.split(",");
+		        jobList.push(row);
+		    }
+
+		    //LOAD stats
+	        var stmt = db.prepare("SELECT * FROM stats_type");
+			stmt.getAsObject();
+			
+			while(stmt.step()) { //
+		        var row = stmt.getAsObject();
+		        statList[row.id] = row;
+		    }
+
+	        app = new FormViewModel();
+			ko.applyBindings(app);
+	    }
+	    r.readAsArrayBuffer(f);
+	});
+
+	var FormViewModel = function() {
+		self = this;
+
+		self.jobList = ko.observableArray(jobList);
+		self.statList = ko.observableArray(statList);
+
+		self.job = ko.observable(null);
+
+		self.getJob = function() {
+			return _.find(self.jobList(), function(item) { return item.id == self.job()})
+		};
+
+		self.getStatById = function(id) {
+			if (!_.findWhere(self.statList(), {id: id*1}))
+			{
+				return false;
+			}
+			return _.findWhere(self.statList(), {id: id*1});
+		}
+
+		self.baseStats = ko.computed(function() {
+			return _.where(self.statList(), {basestat: 1})
+		});
+
+		self.name = ko.computed(function() {
+			if (!self.job()) {
+				return "";
+			}
+			return self.getJob().name
+		});
+
+		self.hitdie = ko.computed(function() {
+			if (!self.job()) {
+				return "";
+			}
+			return self.getJob().hitdie
+		});
+
+		self.statPriority = ko.computed(function() {
+			if (!self.job()) {
+				return [];
+			}
+			return self.getJob().statpriority;
+		});
+
+		self.selectedNewStat = ko.observable();
+
+		self.addPriority = function() {
+			self.statPriority().push(self.selectedNewStat());
+		}
+	};
+});
