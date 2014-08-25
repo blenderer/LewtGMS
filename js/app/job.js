ee93a9fb73742f81ba12ac0e35eb0086cc0ddfc3
Eric Harrison <eric.h@rrison.io>
2014-08-25 08:30:01 -0400

    Document it up a bit

--- js/app/job.js
+++ js/app/job.js
@@ -8,23 +8,29 @@ require([
 	'underscore'
 	], function ($, jui, SQL, ko, komapping, kosort, _) {
 
+		//When we add a file to the uploader, do the following
 		$("#fileupload").change(function() {
 			$("#form").show();
 			var f = $(this).prop("files")[0];
 		    var r = new FileReader();
 		    r.onload = function() {
 		        var Uints = new Uint8Array(r.result);
+		        //Turn our file into a sqlite database in memory
 		        db = new SQL.Database(Uints);
 
+		        //Make our viewmodel global, so we can easily debug in chrome
 		        viewModel = viewModel();
+		        //Then apply the binding
 		        ko.applyBindings(viewModel);
 		    }
 		    r.readAsArrayBuffer(f);
 		});
 
-		viewModel = function() {
+		var viewModel = function() {
+			//get our data from the db first
 			var data = loadJob();
 
+			//These properties need to not be observable
 			var options = {
 			    'copy': [
 			    	"statlist",
@@ -34,13 +40,17 @@ require([
 			    ]
 			}
 
+			//Apply our ko.mapping
 			self = komapping.fromJS(data, options);
 
+			//Observables for when someone uses a bound select
 			self.selectedStat = ko.observable();
 			self.selectedJob = ko.observable();
 
+			//All of the basestats: (str, dex, int, etc...)
 			self.baseStats = ko.observableArray(_.where(self.statlist, {basestat: 1}));
 
+			//Adds a stat priority to the stat priority observable array
 			self.addPriority = function() {
 				if (!_.contains(self.properties.statpriority(), self.selectedStat())) {
 					self.properties.statpriority.push(self.selectedStat());
@@ -50,8 +60,10 @@ require([
 				}
 			}
 
+			//Gets bound to an anchor tag for download the saved DB
 			self.saveLink = ko.observable();
 
+			//Remaps the new data from a new job (Or the same one)
 			self.changeJob = function() {
 				komapping.fromJS(loadJob(self.selectedJob()), self)
 			}
@@ -60,6 +72,7 @@ require([
 				self.properties.statpriority.remove(stat);
 			}
 
+			//Returns the job back to its "clean" state (Which we have saved)
 			self.resetJob = function() {
 				_.each(JSON.parse(self.clean), function(value, key) {
 					self.properties[key](value);
@@ -67,7 +80,10 @@ require([
 			}
 
 			self.saveJob = function() {
+				//makes our properties easy to work with
 				var props = ko.toJS(self.properties);
+
+				//Recreates our comma-delimted id string for db insert
 				var idstatpriority = _.map(props.statpriority, function(longname) {
 							return _.findWhere(self.statlist, {longname: longname}).id;
 						}).join();
@@ -80,12 +96,17 @@ require([
 
 				db.run(sql);
 
+				//File creation magic
 				var blob = new Blob([db.export()], {type: "application/octet-stream"});
 	        	self.saveLink(window.URL.createObjectURL(blob));
 
+	        	//Reload the selected job so our viewmodel is clean
 	        	self.changeJob();
 			}
 
+			//Compares the current viewmodel to the original state on load
+			//We use a rateLimit extension because for some reason it was calculating
+			//too fast after we saved
 			self.isDirty = ko.computed(function() {
 				return JSON.stringify(ko.toJS(self.properties)) != self.clean;
 			}).extend({ rateLimit: 10 });
@@ -94,18 +115,24 @@ require([
 			return self;
 		}
 
+		/**
+		 * Loads a job as a js object from the sqlite database
+		 * Accepts a job id number as a parameter
+		 *
+		 */ 
 		function loadJob(job) {
-			//LOAD JOB NAMES
+			//LOAD JOB NAMES/IDs
 	        var stmt = db.prepare("SELECT id, name FROM Jobs");
 			stmt.getAsObject();
 
 			var jobs = [];
 			
-			while(stmt.step()) { //
+			while(stmt.step()) {
 		        var row = stmt.getAsObject();
 		        jobs.push(row);
 		    }
 
+		    //If no job id provided, use the first one from the list
 		    var jobId = job || jobs[0].id;
 
 		    //GET STAT NAMES
@@ -114,7 +141,7 @@ require([
 	        var stmt = db.prepare("SELECT * FROM stats_type");
 			stmt.getAsObject();
 			
-			while(stmt.step()) { //
+			while(stmt.step()) {
 		        var row = stmt.getAsObject();
 		        statList.push(row);
 		    }
@@ -125,16 +152,19 @@ require([
 
 			var properties = [];
 			
-			while(stmt.step()) { //
+			while(stmt.step()) {
 		        var row = stmt.getAsObject();
+		        //turn our comma delimted string into an array
 		        row.statpriority = row.statpriority.split(",");
 		        properties = row;
 		    }
 
+		    //Then step through the array and map it to its human-readable name
 		    for (var i=0; i<properties.statpriority.length; i++) {
 		    	properties.statpriority[i] = _.findWhere(statList, {id: properties.statpriority[i]*1}).longname;
 		    }
 
+		    //Return our object which will be consumed by the ViewModel
 		    return {
 		    	jobs: jobs,
 		    	properties: properties,
