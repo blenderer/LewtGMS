adb66afe50ab0b9f836745f9343a0f24654c1df6
Eric Harrison <eric.h@rrison.io>
2014-11-04 12:18:41 -0500

    Refactored stat.js for json text file loading rather than sqlite3.

--- stat/app/stat.js
+++ stat/app/stat.js
@@ -1,11 +1,10 @@
 require([
-	'sql',
 	'knockout',
 	'komapping',
 	'kosortable',
 	'underscore',
 	'lewtgms.common'
-	], function (SQL, ko, komapping, kosort, _, common) {
+	], function (ko, komapping, kosort, _, common) {
 
 		var uploader = document.querySelector("#fileupload input");
 		//When we add a file to the uploader, do the following
@@ -14,158 +13,61 @@ require([
 			var f = this.files[0];
 		    var r = new FileReader();
 		    r.onload = function() {
-		        var Uints = new Uint8Array(r.result);
-		        //Turn our file into a sqlite database in memory
-		        db = new SQL.Database(Uints);
-
 		        //Make our viewmodel global, so we can easily debug in chrome
-		        viewModel = viewModel();
+		        //viewModel = viewModel(JSON.parse(r.result));
 		        //Then apply the binding
-		        ko.applyBindings(viewModel, formElement);
+		        ko.applyBindings(viewModel(JSON.parse(r.result), formElement));
 		        formElement.style.display = "block";
 		        uploader.style.display = "none";
 		    }
-		    r.readAsArrayBuffer(f);
+		    r.readAsText(f);
 		}
 
 		uploader.addEventListener('change', initInterface);
 
-		viewModel = function() {
-			//get our data from the db first
-			var data = loadStats();
+		viewModel = function(data) {
 
-			//These properties need to not be observable
-			var options = {
-			    /*'copy': [
-			    	"clean"
-			    ]*/
+			for (var i=0; i<data.stats.length; i++) {
+				data.stats[i].selected = false;
 			}
 
-			//Apply our ko.mapping
-			self = komapping.fromJS(data, options);
+			data.stats[0].selected = true;
 
-			self.selectedStat = ko.observable();
+			var self = komapping.fromJS(data);
+
+			self.baseFields = ["short", "long", "base"];
+
+			self.originalProps = function(){ return _.map(ko.toJS(self).stats, function(stat) {
+				var returnSet = {};
+
+				for (var i=0; i<self.baseFields.length; i++) {
+					var property = self.baseFields[i]
+					returnSet[property] = stat[property];
+				}
+
+				return returnSet;
+			})};
+
+			self.selectedStat = ko.observable("");
 
 			//Gets bound to an anchor tag for download the saved DB
 			self.saveLink = ko.observable();
 
-			//Remaps the new data from a new job (Or the same one)
-			self.changeStat = function() {
-				komapping.fromJS(loadStats(self.selectedStat()), self)
+			self.changeSelected = function() {
+				for (var i=0; i<data.stats.length; i++) {
+					self.stats()[i].selected(false);
+				}
+				_.find(self.stats(), function(stat) {
+					return stat.long() == self.selectedStat();
+				}).selected(true);
 			}
 
-			self.removeStatAndRefs = function() {
-				if (window.confirm("Do you really want to delete this stat?")) { 
-					var sql = "DELETE FROM Stats_type where id=" + self.id();
-					db.run(sql);
-
-					var sql2 = "DELETE FROM Job_priorities where stat_id = " + self.id();
-					db.run(sql2);
-
-					var sql3 = "DELETE FROM Job_secondaries where stat_id = " + self.id();
-					db.run(sql3);
-				}
-			}
-
-			self.getSavableProperties = function() {
-				var secondary = 0;
-				var base = 0;
-				if (self.type() == 'secondary') {
-					secondary = 1;
-				}
-				if (self.type() == 'base') {
-					base = 1;
-				}
-				return {
-					id: self.id(),
-					longname: self.longname(),
-					shortname: self.shortname(),
-					basestat: base,
-					secondary: secondary
-				}
-			}
-
-			self.saveStat = function() {
-				//makes our properties easy to work with
-				var currentStat = self.getSavableProperties();
-
-				var sql = "INSERT OR REPLACE INTO Stats_type" +
-				"(id, shortname, longname, basestat, secondary)" +
-				"VALUES ('"+currentStat.id+"', '"+currentStat.shortname+"', '"+currentStat.longname+
-					"', '"+currentStat.basestat+"', '"+currentStat.secondary+"')";
-
-				
-
-				db.run(sql);
-
+			self.save = function() {
 				//File creation magic
-				var blob = new Blob([db.export()], {type: "application/octet-stream"});
+				var blob = new Blob([JSON.stringify({"stats": self.originalProps()}, null, '    ')], {type: "application/octet-stream"});
 	        	self.saveLink(window.URL.createObjectURL(blob));
-
-	        	//Reload the selected job so our viewmodel is clean
-	        	self.changeStat();
 			}
-
-			//Compares the current viewmodel to the original state on load
-			//We use a rateLimit extension because for some reason it was calculating
-			//too fast after we saved
-			self.isDirty = ko.computed(function() {
-				return JSON.stringify(ko.toJS(
-					{
-						id: self.id(),
-						shortname: self.shortname(),
-						longname: self.longname(),
-						type: self.type()
-					}
-					)) != self.clean();
-			}).extend({ rateLimit: 10 });
 
 			return self;
-		}
-
-		/**
-		 * Loads a job as a js object from the sqlite database
-		 * Accepts a job id number as a parameter
-		 *
-		 */ 
-		function loadStats(stat) {
-			//LOAD JOB NAMES/IDs
-	        var stmt = db.prepare("SELECT * FROM stats_type");
-			stmt.getAsObject();
-
-			var stats = [];
-			
-			while(stmt.step()) {
-		        var row = stmt.getAsObject();
-		        stats.push(row);
-		    }
-
-		    //If no job id provided, use the first one from the list
-		    var statId = stat || stats[0].id;
-
-		    var statObject = _.findWhere(stats, {id: statId});
-
-		    if (statObject.basestat) {
-		    	statObject.type = "base";
-		    }
-		    else if(statObject.secondary) {
-		    	statObject.type = "secondary";
-		    }
-		    else {
-		    	statObject.type = "none";
-		    }
-
-		    delete statObject.basestat;
-		    delete statObject.secondary;
-
-		    //Return our object which will be consumed by the ViewModel
-		    return {
-		    	stats: stats,
-		    	id: statObject.id,
-		    	shortname: statObject.shortname,
-		    	longname: statObject.longname,
-		    	type: statObject.type,
-		    	clean: JSON.stringify(stats)
-		    };
 		}
 });
