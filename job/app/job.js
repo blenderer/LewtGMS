1a7ca8c4919a4222d34fd6efd3044636288e5cc5
Eric Harrison <eric.h@rrison.io>
2014-11-07 12:47:58 -0500

    Commonize viewModel loading and fileupload magic

--- job/app/job.js
+++ job/app/job.js
@@ -5,107 +5,90 @@ require([
 	'lewtgms.common'
 	], function (ko, komapping, _, common) {
 
-		var uploader = document.querySelector("#fileupload input");
-		//When we add a file to the uploader, do the following
-		var initInterface = function() {
-			var formElement = document.getElementById('form');
-			var f = this.files[0];
-		    var r = new FileReader();
-		    r.onload = function() {
-		        //Make our viewmodel global, so we can easily debug in chrome
-		        //viewModel = viewModel(JSON.parse(r.result));
-		        //Then apply the binding
+	viewModel = function(data) {
+		//map the original properties so we dont add in unecessary IDs and selected stuff
+		data.originalProps = common.getPropertyMap(data);
 
-		        ko.applyBindings(viewModel(JSON.parse(r.result)), formElement);
-		        formElement.style.display = "block";
-		        uploader.style.display = "none";
-		    }
-		    r.readAsText(f);
+		//originalProps doesn't need to be observable
+		var options = {
+			"copy": ["originalProps"]
+		};
+
+		//bind the data to the viewmodel
+		var self = komapping.fromJS(data, options);
+
+		//Add in some observables to help us order and select things on the UI
+		for (var i=0; i<self.jobs().length; i++) {
+			self.jobs()[i].selected = ko.observable(false)
+			self.jobs()[i].id = ko.observable(i);
+
+			self.jobs()[i].removePriority = function() {
+
+			}
+
+			self.jobs()[i].movePriorityUp = function() {
+
+			}
+
+			self.jobs()[i].movePriorityDown = function() {
+
+			}
+
+			for (var secondary=0; secondary<self.jobs()[i].secondaries().length; secondary++) {
+				self.jobs()[i].secondaries()[secondary].removeSecondary = function() {
+					
+				}
+			}
 		}
+		self.jobs()[0].selected(true);
+		self.selectedJob = ko.observable("");
 
-		uploader.addEventListener('change', initInterface);
+		//Gets bound to an anchor tag for download the saved DB
+		self.saveLink = ko.observable();
 
-		viewModel = function(data) {
-			//map the original properties so we dont add in unecessary IDs and selected stuff
-			data.originalProps = common.getPropertyMap(data);
-
-			//originalProps doesn't need to be observable
-			var options = {
-				"copy": ["originalProps"]
-			};
-
-			//bind the data to the viewmodel
-			var self = komapping.fromJS(data, options);
-
-			//Add in some observables to help us order and select things on the UI
+		self.changeSelectedJob = function() {
 			for (var i=0; i<self.jobs().length; i++) {
-				self.jobs()[i].selected = ko.observable(false)
-				self.jobs()[i].id = ko.observable(i);
-
-				self.jobs()[i].removePriority = function() {
-
-				}
-
-				self.jobs()[i].movePriorityUp = function() {
-
-				}
-
-				self.jobs()[i].movePriorityDown = function() {
-
-				}
-
-				for (var secondary=0; secondary<self.jobs()[i].secondaries().length; secondary++) {
-					self.jobs()[i].secondaries()[secondary].removeSecondary = function() {
-						
-					}
-				}
-			}
-			self.jobs()[0].selected(true);
-			self.selectedJob = ko.observable("");
-
-			//Gets bound to an anchor tag for download the saved DB
-			self.saveLink = ko.observable();
-
-			self.changeSelectedJob = function() {
-				for (var i=0; i<self.jobs().length; i++) {
-					self.jobs()[i].selected(false);
-				}
-
-				_.find(self.jobs(), function(job) {
-					return job.id() == self.selectedJob()
-				}).selected(true)
+				self.jobs()[i].selected(false);
 			}
 
-			self.newJob = function() {
-				var newId = self.jobs().length + 1;
-				self.stats.push({
-					"name": ko.observable("{Enter Name}"),
-					"id": ko.observable(newId),
-					"statpriorities": ko.observableArray(),
-					"secondaries": ko.observableArray()
-				});
-				self.selectedJob(newId);
-				self.changeSelectedJob();
-			}
-
-			self.removeSelectedJob = function() {
-				self.jobs.remove(_.find(self.jobs(), function(job) {
-					return job.selected()
-				}));
-			}
-
-			self.save = function() {
-				//File creation magic
-				var originalProps = self.originalProps;
-				delete self.originalProps;
-				var removed = common.removeAppSpecificProperties(komapping.toJS(self), originalProps);
-
-				var blob = new Blob([JSON.stringify(removed, null, '    ')], {type: "application/octet-stream"});
-	        	self.saveLink(window.URL.createObjectURL(blob));
-
-	        	self.originalProps = originalProps;
-			}
-
-			return self;
+			_.find(self.jobs(), function(job) {
+				return job.id() == self.selectedJob()
+			}).selected(true)
 		}
+
+		self.newJob = function() {
+			var newId = self.jobs().length + 1;
+			self.stats.push({
+				"name": ko.observable("{Enter Name}"),
+				"id": ko.observable(newId),
+				"statpriorities": ko.observableArray(),
+				"secondaries": ko.observableArray()
+			});
+			self.selectedJob(newId);
+			self.changeSelectedJob();
+		}
+
+		self.removeSelectedJob = function() {
+			self.jobs.remove(_.find(self.jobs(), function(job) {
+				return job.selected()
+			}));
+		}
+
+		self.save = function() {
+			//File creation magic
+			var originalProps = self.originalProps;
+			delete self.originalProps;
+			var removed = common.removeAppSpecificProperties(komapping.toJS(self), originalProps);
+
+			var blob = new Blob([JSON.stringify(removed, null, '    ')], {type: "application/octet-stream"});
+        	self.saveLink(window.URL.createObjectURL(blob));
+
+        	self.originalProps = originalProps;
+		}
+
+		return self;
+	}
+
+	//Attach the viewModel to the form and intiate it on fileupload
+	common.uploaderTie("#fileupload input", viewModel, "#form");
 });
--- stat/app/stat.js
+++ stat/app/stat.js
@@ -5,90 +5,73 @@ require([
 	'lewtgms.common'
 	], function (ko, komapping, _, common) {
 
-		var uploader = document.querySelector("#fileupload input");
-		//When we add a file to the uploader, do the following
-		var initInterface = function() {
-			var formElement = document.getElementById('form');
-			var f = this.files[0];
-		    var r = new FileReader();
-		    r.onload = function() {
-		        //Make our viewmodel global, so we can easily debug in chrome
-		        //viewModel = viewModel(JSON.parse(r.result));
-		        //Then apply the binding
 
-		        ko.applyBindings(viewModel(JSON.parse(r.result)), formElement);
-		        formElement.style.display = "block";
-		        uploader.style.display = "none";
-		    }
-		    r.readAsText(f);
+	viewModel = function(data) {
+		//map the original properties so we dont add in unecessary IDs and selected stuff
+		data.originalProps = common.getPropertyMap(data);
+
+		//originalProps doesn't need to be observable
+		var options = {
+			"copy": ["originalProps"]
+		};
+
+		//bind the data to the viewmodel
+		var self = komapping.fromJS(data, options);
+
+		//Add in some observables to help us order and select things on the UI
+		for (var i=0; i<self.stats().length; i++) {
+			self.stats()[i].selected = ko.observable(false)
+			self.stats()[i].id = ko.observable(i);
 		}
+		self.stats()[0].selected(true);
+		self.selectedStat = ko.observable("");
 
-		uploader.addEventListener('change', initInterface);
+		//Gets bound to an anchor tag for download the saved DB
+		self.saveLink = ko.observable();
 
-		viewModel = function(data) {
-			//map the original properties so we dont add in unecessary IDs and selected stuff
-			data.originalProps = common.getPropertyMap(data);
-
-			//originalProps doesn't need to be observable
-			var options = {
-				"copy": ["originalProps"]
-			};
-
-			//bind the data to the viewmodel
-			var self = komapping.fromJS(data, options);
-
-			//Add in some observables to help us order and select things on the UI
+		self.changeSelectedStat = function() {
 			for (var i=0; i<self.stats().length; i++) {
-				self.stats()[i].selected = ko.observable(false)
-				self.stats()[i].id = ko.observable(i);
-			}
-			self.stats()[0].selected(true);
-			self.selectedStat = ko.observable("");
-
-			//Gets bound to an anchor tag for download the saved DB
-			self.saveLink = ko.observable();
-
-			self.changeSelectedStat = function() {
-				for (var i=0; i<self.stats().length; i++) {
-					self.stats()[i].selected(false);
-				}
-
-				_.find(self.stats(), function(stat) {
-					return stat.id() == self.selectedStat()
-				}).selected(true)
+				self.stats()[i].selected(false);
 			}
 
-			self.newStat = function() {
-				var newId = self.stats().length + 1;
-				self.stats.push({
-					"long": ko.observable("{Enter Name}"),
-					"short": ko.observable(""),
-					"id": ko.observable(self.stats().length + 1),
-					"base": ko.observable(false),
-					"selected": ko.observable(false)
-				});
-				self.selectedStat(newId);
-				self.changeSelectedStat();
-			}
-
-			self.removeSelectedStat = function() {
-				self.stats.remove(_.find(self.stats(), function(stat) {
-					return stat.selected()
-				}));
-			}
-
-			self.save = function() {
-				//File creation magic
-				var originalProps = self.originalProps;
-				delete self.originalProps;
-				var removed = common.removeAppSpecificProperties(komapping.toJS(self), originalProps);
-
-				var blob = new Blob([JSON.stringify(removed, null, '    ')], {type: "application/octet-stream"});
-	        	self.saveLink(window.URL.createObjectURL(blob));
-
-	        	self.originalProps = originalProps;
-			}
-
-			return self;
+			_.find(self.stats(), function(stat) {
+				return stat.id() == self.selectedStat()
+			}).selected(true)
 		}
+
+		self.newStat = function() {
+			var newId = self.stats().length + 1;
+			self.stats.push({
+				"long": ko.observable("{Enter Name}"),
+				"short": ko.observable(""),
+				"id": ko.observable(self.stats().length + 1),
+				"base": ko.observable(false),
+				"selected": ko.observable(false)
+			});
+			self.selectedStat(newId);
+			self.changeSelectedStat();
+		}
+
+		self.removeSelectedStat = function() {
+			self.stats.remove(_.find(self.stats(), function(stat) {
+				return stat.selected()
+			}));
+		}
+
+		self.save = function() {
+			//File creation magic
+			var originalProps = self.originalProps;
+			delete self.originalProps;
+			var removed = common.removeAppSpecificProperties(komapping.toJS(self), originalProps);
+
+			var blob = new Blob([JSON.stringify(removed, null, '    ')], {type: "application/octet-stream"});
+        	self.saveLink(window.URL.createObjectURL(blob));
+
+        	self.originalProps = originalProps;
+		}
+
+		return self;
+	}
+	//Attach the viewModel to the form and intiate it on fileupload
+	common.uploaderTie("#fileupload input", viewModel, "#form");
 });
